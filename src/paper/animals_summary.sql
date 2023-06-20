16f521251541743321a1734dbf7bd39e5d4086a8
Aaron Benson Wong <aaronbwong+github@proton.me>
2023-06-20 14:45:55 +0200

    added ephysID to animal summary

--- src/paper/animals_summary.sql
+++ src/paper/animals_summary.sql
@@ -1,5 +1,5 @@
 -- Animals used in behavior
-SELECT animalid,sex,construct_name,dob FROM mice.animals WHERE animalid IN 
+SELECT animalid,sex,construct_name,dob,ephysid FROM mice.animals WHERE animalid IN 
 ('19-MI16506-02',
 '19-MI16506-03',
 '19-MI17966-03',
@@ -9,14 +9,14 @@ SELECT animalid,sex,construct_name,dob FROM mice.animals WHERE animalid IN
 
 
 -- Animals used in electrophysiology with units with baseline+salicylate conditions
-SELECT animalid,sex,construct_name,dob FROM mice.animals WHERE ephysid IN 
+SELECT animalid,sex,construct_name,dob,ephysid FROM mice.animals WHERE ephysid IN 
 ('M25', 'M26', 'M27', 'M29', 
  'M30', 'M42', 'M43', 
 'M71',  'M72', 'M74');
 
 -- Counting the sexes from the data above
 SELECT sex, count(animalid) FROM 
-(SELECT animalid,sex,construct_name,dob FROM mice.animals WHERE ephysid IN 
+(SELECT animalid,sex FROM mice.animals WHERE ephysid IN 
 ('M25', 'M26', 'M27', 'M29', 
  'M30', 'M42', 'M43', 
 'M71',  'M72', 'M74')
