0d816336783baf6d1b80becd06772929f370d7c9
Aaron Benson Wong <a.wong@erasmusmc.nl>
2023-08-17 22:19:42 +0200

    updated figure 4 code

--- src/paper/datapaths.m
+++ src/paper/datapaths.m
@@ -1,4 +1,5 @@
 FRAPath = 'W:\AW_repository_transfer\Maurits\FRA_analysis';
 PopPath = 'W:\AW_repository_transfer\Maurits\Population';
 SumPath = '.\Data\Neuro';
+BehPath = '.\Data\Behavior';
 FigPath = '.\Figures\Links';
