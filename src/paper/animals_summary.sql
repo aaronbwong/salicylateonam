218809f216f8274f0167531ce62a51a744b2947c
Aaron Benson Wong <a.wong@erasmusmc.nl>
2023-11-29 13:46:49 +0100

    Behaveior animal summary

--- src/paper/animals_summary.sql
+++ src/paper/animals_summary.sql
@@ -23,3 +23,16 @@ SELECT sex, count(animalid) FROM
  ) AS tb
  GROUP BY sex;
  
+ -- Animals used in both behavior and in electrophysiology with units with baseline+salicylate conditions
+SELECT animalid,sex,construct_name,dob,ephysid FROM mice.animals WHERE animalid IN 
+('19-MI16506-02',
+'19-MI16506-03',
+'19-MI17966-03',
+'19-MI17966-04',
+'20-MI10996-01',
+'20-MI10996-02')
+AND 
+ephysid IN 
+('M25', 'M26', 'M27', 'M29', 
+ 'M30', 'M42', 'M43', 
+'M71',  'M72', 'M74');
